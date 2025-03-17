dfe38c9a404629a3d2a7a0b9f452409e27e7cb1f
F415alarrahman <f415alarr@gmail.com>
2025-03-17 08:58:24 +0700

    refactor: clean up main.dart by removing unnecessary HomeController instantiation

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 import 'package:getx/bindings/home_binding.dart';
-import 'package:getx/controllers/home_controller.dart';
 import 'package:getx/views/home_screen.dart';
 import 'package:getx/views/screen2.dart';
 
@@ -10,17 +9,16 @@ void main() {
 }
 
 class MyApp extends StatelessWidget {
-  final counterC = Get.put(HomeController());
   @override
   Widget build(BuildContext context) {
-    return Obx(() => GetMaterialApp(
-          theme: ThemeData.light(),
-          debugShowCheckedModeBanner: false,
-          initialRoute: '/',
-          getPages: [
-            GetPage(name: '/', page: () => HomePage(), binding: HomeBinding()),
-            GetPage(name: '/second', page: () => Sceen2())
-          ],
-        ));
+    return GetMaterialApp(
+      theme: ThemeData.light(),
+      debugShowCheckedModeBanner: false,
+      initialRoute: '/',
+      getPages: [
+        GetPage(name: '/', page: () => HomePage(), binding: HomeBinding()),
+        GetPage(name: '/second', page: () => Sceen2())
+      ],
+    );
   }
 }
